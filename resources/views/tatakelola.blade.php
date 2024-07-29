9b5cb162c4b67890013b7f15cb977fae83a83148
Irvan628 <irvan.nurfauzansaputra@student.upj.ac.id>
2024-07-29 08:15:44 +0700

    update

--- resources/views/tatakelola.blade.php
+++ resources/views/tatakelola.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Bootstrap Demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <style>
@@ -21,7 +21,6 @@
             padding: 40px 0;
         }
         #portal {
-            
             color: #124173;
             font-weight: bold;
         }
@@ -59,9 +58,55 @@
             color: #124173;
             font-weight: bold;
         }
+        .navbar-dark .navbar-nav .nav-link {
+            color: rgb(255, 255, 255);
+            font-size: 20px;
+        }
+        .navbar-dark .navbar-nav .nav-link:hover {
+            color: rgba(255, 255, 255, 0.541);
+        }
     </style>
   </head>
   <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="{{ asset('/assets/images/bg/APMI.png') }}" width="30" height="30" class="d-inline-block align-top" alt="">
+          Sistem Tata Kelola APMI
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#tatakelola">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Keanggotaan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Kegiatan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Sekretariat</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Keuangan</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Arsip</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Tentang</a>
+              </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Sections -->
     <section id="section1">
         <h1 id="text-1">#MembumikanPenelitian</h1>
     </section>
@@ -77,7 +122,6 @@
             Setiap komunitas memerlukan sistem yang baik dalam pelaksanaannya. APMI menerapkan sistem Tata Kelola yang diadaptasi dari prinsip good organization governance (GOG). Adapun lima pilar utama GOG yaitu transparency (transparansi), accountability (akuntabilitas), responsibility (responsibilitas), independence (kebebasan), dan fairness (keadilan).
             <br>
             <br>
-
             Sistem Tata Kelola ini dipadukan menjadi satu portal manajemen yang mana seluruh anggota APMI diwajibkan untuk menggunakan portal website ini untuk mengakses, menginput data, melihat agenda, dan hal-hal lainnya yang berkaitan. Secara garis besar, portal ini dapat diakses untuk keanggotaan, agenda kegiatan, kemitraan, secretariat, keuangan, dan arsip.
           </p>
         </div>
